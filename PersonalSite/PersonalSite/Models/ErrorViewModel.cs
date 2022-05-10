132ea8be54d72b48aa75fa3e7a8e7746ab5e4d3c
Tanner Miller <tmmiller2@outlook.com>
2022-05-10 15:29:49 -0500

    imported all files to start implementing MVC to Personal Site

---
+++ PersonalSite/PersonalSite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PersonalSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
